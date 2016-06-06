747edd1b63850145685ed3a4d869ca5543944e5a
Jon Edwards <jkedwards@me.com>
2016-06-06 23:48:22 +0000

    add get parser and structure for both put and get

--- snippets.py
+++ snippets.py
@@ -35,7 +35,20 @@ def main():
     put_parser.add_argument('name', help = 'Name of the snippet')
     put_parser.add_argument('snippet', help = 'Snippet text')
     
+    get_parser = subparsers.add_parser('get', help = 'Retrieve a snippet')
+    get_parser.add_argument('name', help = 'Name of the snippet')
+
     arguments = parser.parse_args()
+    
+    arguments = vars(arguments)
+    command = arguments.pop('command')
+    
+    if command == 'put':
+        name, snippet = put(**arguments)
+        print('Store {!r} as {!r}'.format(snippet, name))
+    elif command == 'get':
+        snippet = get(**arguments)
+        print('Retrieved snippet: {!r}'.format(snippet))
     
 if __name__ == '__main__':
     main()
